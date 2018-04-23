0ed84887074d64870e7d595d5bb756fa9b9f3a34
Rafael Gibam <rafael.w.silva@hotmail.com>
2018-04-23 11:54:40 -0300

    Implementando estoque

---
+++ Pages/estoque.php
@@ -0,0 +1,37 @@
+<div class="container mt-5">
+    <div class="row text-center">
+        <div class="col-md-4" align="center">
+            <div class="card card-person" style="width: 18rem;">
+                <i class="ion-android-happy"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Gerente</h5>
+                    <p class="card-text">Lista de gerentes cadastrados no sistema.</p>
+                    <a href="/gerente" class="btn btn-primary btn-ver">Ver Gerentes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4" align="center">
+            <div class="card card-person" style="width: 18rem;">
+                <i class="ion-help-buoy"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Atendente</h5>
+                    <p class="card-text">Lista de atendentes cadastrados no sistema.</p>
+                    <a href="/atendente" class="btn btn-primary btn-ver">Ver atendentes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4" align="center">
+            <div class="card card-person" style="width: 18rem;">
+                <i class="ion-paintbrush"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Arte Finalista</h5>
+                    <p class="card-text">Lista de arte finalistas cadastrados no sistema.</p>
+                    <a href="/artefinalista" class="btn btn-primary btn-ver">Ver arte finalistas</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
